d33c8e71814d58c6a41a714ba8583d51e54d7207
William Liao <carzywilliamliao@gmail.com>
2021-02-07 08:58:42 +0800

    Initial Commit

---
+++ Jikan/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Jikan
+//
+//  Created by William Liao on 2021/2/7.
+//  Copyright © 2021 William Liao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
